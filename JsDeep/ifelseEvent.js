c1781752788c4023d3b8c3399837c61201898ff1
rickstaryo <qnrud16@gmail.com>
2022-07-03 23:51:35 +0900

    2022-07-03

--- JsDeep/ifelseEvent.js
+++ JsDeep/ifelseEvent.js
@@ -1,8 +1,9 @@
 const h1= document. querySelector("div.hello:first-child h1")
 
 function handleTitleClick(){
+    const currentColor = h1.style.color;
     let newColor;
-    if(h1.style.color == "blue" ){
+    if( currentColor== "blue" ){
     newColor="tomato";
     }
     else{
@@ -12,3 +13,8 @@ function handleTitleClick(){
     
 }
 h1.addEventListener("click", handleTitleClick);
+
+//  Wrapping up 
+//  Step.1 search the elements of thing
+//  Step.2 event listening 
+//  step.3 react that events
