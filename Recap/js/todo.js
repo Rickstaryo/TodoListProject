9628949c1fc31803a7074a555876b6dfd1131125
rickstaryo <qnrud16@gmail.com>
2022-08-13 22:59:54 +0900

    Delete button on List up
    1.Need to understand filter() 2.Build up the Delete button

--- Recap/js/todo.js
+++ Recap/js/todo.js
@@ -2,7 +2,8 @@ const toDoForm =document.getElementById("todo-form");
 const toDoInput= document.querySelector("#todo-form input");
 const toDoList=document.getElementById("todo-list");
 
-const toDos =[];
+const TODOS_KEY="todos";
+let toDos =[];
 
 function saveToDos() {
     localStorage.setItem("todos", JSON.stringify(toDos));
@@ -11,12 +12,16 @@ function saveToDos() {
 function deleteToDo(event){
     const li = event.target.parentElement;
     li.remove();
+  
+    toDos = toDos.filter(toDo=> toDo.id !==parseInt(li.id));
+    saveToDos();
 }
 
 function paintToDo(newTodo){
     const li  = document.createElement("li");
+    li.id= newTodo.id;
     const span=document.createElement("span");
-    span.innerText=newTodo;
+    span.innerText=newTodo.text;
     const button = document. createElement("button");
     button.innerText="❌"
     button.addEventListener("click",deleteToDo);
@@ -31,9 +36,26 @@ function handleTodoList(event){
     event.preventDefault();
     const newTodo= toDoInput.value;
     toDoInput.value="";
-    toDos.push(newTodo);
-    paintToDo(newTodo);
+    const newTodoObj={
+        id: Date.now(),
+        text: newTodo
+    }
+
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
     saveToDos();
 }
 
 toDoForm.addEventListener("submit",handleTodoList);
+
+function sayHello(item){
+    console.log("Hello");
+}
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if(savedToDos !== null){
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+}
